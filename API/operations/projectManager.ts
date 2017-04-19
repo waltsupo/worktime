18dfeacfefd92f6aa9f39e64682c7849b51fb7b5
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2017-04-19 13:59:07 +0300

    Add project manager with routes

---
+++ API/operations/projectManager.ts
@@ -0,0 +1,22 @@
+export class ProjectManager {
+
+    public getProjectWithId = (req: express.Request, res: express.Response) => {
+
+    };
+
+    public getProjectWithIdentifier = (req: express.Request, res: express.Response) => {
+
+    };
+
+    public createProject = (req: express.Request, res: express.Response) => {
+
+    };
+
+    public modifyProject = (req: express.Request, res: express.Response) => {
+
+    };
+
+    public deleteProject = (req: express.Request, res: express.Response) => {
+
+    };
+}
--- API/routes.ts
+++ API/routes.ts
@@ -1,15 +1,20 @@
 import * as express from 'express';
+import {ProjectManager} from "./operations/projectManager";
 
 export class Routes {
     private router: express.Router;
+    private projectManager: ProjectManager;
 
     constructor() {
         this.router = express.Router();
 
         // Paths
-        this.router.get('/', function(req, res) {
-            res.send('Am the home page!');
-        });
+        this.router.post('/projects/id/:id', this.projectManager.getProjectWithId());
+        this.router.post('/projects/:identifier', this.projectManager.getProjectWithIdentifier());
+        this.router.post('/projects', this.projectManager.createProject());
+        this.router.post('/projects/:id', this.projectManager.modifyProject());
+        this.router.post('/projects/:id', this.projectManager.deleteProject());
+
     }
 
     public getRouter() {
