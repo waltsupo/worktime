df79c0b6872449e1b0bbb6c5f67b0cd96f44886f
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2017-04-19 19:10:46 +0300

    Add configurations(only for returning the files later)

---
+++ API/config/configuration.ts
@@ -0,0 +1,12 @@
+// Visible on github for returning this project and getting the system work properly.
+// Database only contains information about this project.
+
+const configuraion = {
+  database: {
+      connectionLimit: 10,
+      host: 'mydb.tamk.fi',
+      user: 'c5vpouta',
+      password: 'Salasana1',
+      database: 'dbc5vpouta1'
+  }
+};
