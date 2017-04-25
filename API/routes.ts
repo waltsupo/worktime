feea85a3b82a9cc057e15d7f97076423a1bf373e
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2017-04-25 14:25:31 +0300

    Add routes for modifying users

--- API/routes.ts
+++ API/routes.ts
@@ -2,16 +2,19 @@ import * as express from 'express';
 import {ProjectManager} from "./operations/projectManager";
 import {Database} from "./config/database";
 import {CategoryManager} from "./operations/categoryManager";
+import {UserManager} from "./operations/userManager";
 
 export class Routes {
     private router: express.Router;
     private projectManager: ProjectManager;
     private categoryManager: CategoryManager;
+    private userManager: UserManager;
 
     constructor(database: Database) {
         this.router = express.Router();
         this.projectManager = new ProjectManager(database);
         this.categoryManager = new CategoryManager(database);
+        this.userManager = new UserManager(database);
 
         // Paths
         this.router.get('/projects/id/:id', this.projectManager.getProjectWithId);
@@ -25,6 +28,12 @@ export class Routes {
         this.router.post('/categories', this.categoryManager.createCategory);
         this.router.put('/categories/:id', this.categoryManager.modifyCategory);
         this.router.delete('/categories/:id', this.categoryManager.deleteCategory);
+
+        this.router.get('/users/:projectId', this.userManager.getUsers);
+        this.router.get('/users/id/:id', this.userManager.getUser);
+        this.router.post('/users', this.userManager.createUser);
+        this.router.put('/users/:id', this.userManager.modifyUser);
+        this.router.delete('/users/:id', this.userManager.deleteUser);
     }
 
     public getRouter() {
