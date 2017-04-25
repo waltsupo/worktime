fba472720a96d24a873e0969ee3670d7fa3e93f5
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2017-04-25 18:32:39 +0300

    Add taskManager and routes for modifying tasks

--- API/routes.ts
+++ API/routes.ts
@@ -3,18 +3,21 @@ import {ProjectManager} from "./operations/projectManager";
 import {Database} from "./config/database";
 import {CategoryManager} from "./operations/categoryManager";
 import {UserManager} from "./operations/userManager";
+import {TaskManager} from "./operations/taskManager";
 
 export class Routes {
     private router: express.Router;
     private projectManager: ProjectManager;
     private categoryManager: CategoryManager;
     private userManager: UserManager;
+    private taskManager: TaskManager;
 
     constructor(database: Database) {
         this.router = express.Router();
         this.projectManager = new ProjectManager(database);
         this.categoryManager = new CategoryManager(database);
         this.userManager = new UserManager(database);
+        this.taskManager = new TaskManager(database);
 
         // Paths
         this.router.get('/projects/id/:id', this.projectManager.getProjectWithId);
@@ -34,6 +37,12 @@ export class Routes {
         this.router.post('/users', this.userManager.createUser);
         this.router.put('/users/:id', this.userManager.modifyUser);
         this.router.delete('/users/:id', this.userManager.deleteUser);
+
+        this.router.get('/tasks/:projectId', this.taskManager.getTasks);
+        this.router.get('/tasks/id/:id', this.taskManager.getTask);
+        this.router.post('/tasks', this.taskManager.createTask);
+        this.router.put('/tasks/:id', this.taskManager.modifyTask);
+        this.router.delete('/tasks/:id', this.taskManager.deleteTask);
     }
 
     public getRouter() {
